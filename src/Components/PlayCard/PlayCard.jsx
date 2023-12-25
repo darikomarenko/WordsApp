4282343213f44a83452b85d1be43044d7a06704a
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2023-12-25 12:19:46 +0300

    add defaultProps

--- src/Components/PlayCard/PlayCard.jsx
+++ src/Components/PlayCard/PlayCard.jsx
@@ -1,26 +1,29 @@
 import React, { useState } from "react";
 import "./playcard.css";
 
-function PlayCard({english, transcription, russian}) {
-    const [flipped, setFlipped] = useState(false);
+function PlayCard({ english = "Ooops, no Word", transcription, russian }) {
+  const [flipped, setFlipped] = useState(false);
 
-    const handleFlip = () => {
-        setFlipped(!flipped);
-    };
-    return (
+  const handleFlip = () => {
+    setFlipped(!flipped);
+  };
+  return (
     <div className="parent-element">
-        <div className={`word-card ${flipped ? " flipped" : ""}`} title="Нажмите на карточку, чтобы увидеть перевод" 
-        onClick={handleFlip}>
-            <div className="front">
-                <div className="english">{english}</div>
-                <div className="transcription">{transcription}</div>
+      <div
+        className={`word-card ${flipped ? " flipped" : ""}`}
+        title="Нажмите на карточку, чтобы увидеть перевод"
+        onClick={handleFlip}
+      >
+        <div className="front">
+          <div className="english">{english}</div>
+          <div className="transcription">{transcription}</div>
         </div>
         <div className="back">
-            <div className="russian">{russian}</div>
-            </div>
+          <div className="russian">{russian}</div>
         </div>
+      </div>
     </div>
-    );
-};
+  );
+}
 
 export default PlayCard;
