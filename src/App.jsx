3560ce026c608968ff50d3098098c5e155313bfb
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2023-12-25 12:04:27 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import WordsList from "./components/WordsList/Words";
+import wordsdata from "./components/words.json";
+import Game from "./components/Game/Game";
+
+function App() {
+  const wordslist = wordsdata;
+  return (
+    <div className="App">
+      <Header />
+      <div className="Content">
+        <WordsList data={wordslist} />
+        <Game data={wordslist} />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
