e11a8c3ca4bd71f863b118f0a78f9f504c62d6b4
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-02-08 14:42:24 +0300

    add edit mode

--- src/Components/WordsList/Words.jsx
+++ src/Components/WordsList/Words.jsx
@@ -1,112 +1,143 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 import './wordslist.css';
 
-const WordsList = ({data}) => {
+const Words = ({ data }) => {
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [editedWords, setEditedWords] = useState([]);
+  const [buttonDisabled, setButtonDisabled] = useState([]);
+  const [message, setMessage] = useState('');
 
-    const [isEditMode, setIsEditMode] = useState(false);
-    const [editedWords, setEditedWords] = useState([]);
+  useEffect(() => {
+    setButtonDisabled(data.map(() => false));
+  }, [data]);
 
-    const toggleEditMode = (index) => {
-        if (!isEditMode) {
-        const edited = data.map((word, i) => {
-            const editedWord = { ...word };
-            editedWords[i] = editedWord;
-            return editedWord;
-        });
-        setEditedWords(edited);
-        }
-        setIsEditMode(!isEditMode);
-    };
-    
-    const handleEnglishChange = (event, index) => {
-        const value = event.target.value;
-        setEditedWords((prev) =>
-        prev.map((word, i) => {
-            if (i === index) {
-            const editedWord = { ...word, english: value };
-            return editedWord;
-            }
-            return word;
-        })
-        );
-    };
+  const displayMessage = (msg) => {
+    setMessage(msg);
+    setTimeout(() => {
+      setMessage('');
+    }, 3000);
+  };
 
-    const handleTranscriptionChange = (event, index) => {
-        const value = event.target.value;
-        setEditedWords((prev) =>
-        prev.map((word, i) => {
-            if (i === index) {
-            const editedWord = { ...word, transcription: value };
-            return editedWord;
-            }
-            return word;
-        })
-        );
-    };
-    
-    const handleRussianChange = (event, index) => {
-        const value = event.target.value;
-        setEditedWords((prev) =>
-        prev.map((word, i) => {
-            if (i === index) {
-            const editedWord = { ...word, russian: value };
-            return editedWord;
-            }
-            return word;
-        })
-        );
-    };
-    
-
-    const handleSaveClick = () => {
-        console.log(editedWords);
-        setIsEditMode(false);
-    };
-    
-    const handleCancelClick = () => {
-        setEditedWords(data.slice());
-        setIsEditMode(false);
-    };
-
-    const renderRows = () => {
-        return data.map((word, index) => {
-        const editedWord = editedWords[index];
-        return (
-            <tr key={index}>
-            <td>{isEditMode ? <input type="text" value={editedWord.english} onChange={(e) => handleEnglishChange(e, index)} /> : word.english}</td>
-            <td>{isEditMode ? <input type="text" value={editedWord.transcription} onChange={(e) => handleTranscriptionChange(e, index)} /> : word.transcription}</td>
-            <td>{isEditMode ? <input type="text" value={editedWord.russian} onChange={(e) => handleRussianChange(e, index)} /> : word.russian}</td>
-            <td>{isEditMode ? <input type="text" value={editedWord.tags} onChange={(e) => handleEnglishChange(e, index)} /> : word.tags}</td>
-
-            <td>
-                {isEditMode ? (
-                <>
-                    <button className="save-button" onClick={handleSaveClick}>Сохранить</button>
-                    <button className="cancel-button" onClick={handleCancelClick}>Отмена</button>
-                </>
-                ) : (
-                <button className="change-button" onClick={() => toggleEditMode(index)}>Изменить</button>
-                )}
-                <button className="delete-button">Удалить</button>
-            </td>
-            </tr>
-        );
-        });
-    };
-    return (
-        <table>
-        <thead>
-            <tr>
-            <th>Слово</th>
-            <th>Транскрипция</th>
-            <th>Перевод</th>
-            <th>Уровень</th>
-            <th width="30%">Действия</th>
-            </tr>
-        </thead>
-        <tbody>{renderRows()}</tbody>
-        </table>
-    );
+  const toggleEditMode = (index) => {
+    if (!isEditMode) {
+      const edited = data.map((word, i) => {
+        const editedWord = { ...word };
+        editedWords[i] = editedWord;
+        return editedWord;
+      });
+      setEditedWords(edited);
     }
+    setIsEditMode(!isEditMode);
+  };
 
-export default WordsList;
+  const handleInputChange = (event, index, property) => {
+    const value = event.target.value;
+    setEditedWords((prev) =>
+      prev.map((word, i) => {
+        if (i === index) {
+          const editedWord = { ...word, [property]: value };
+          return editedWord;
+        }
+        return word;
+      }),
+    );
+    setButtonDisabled((prev) =>
+      prev.map((disabled, i) =>
+        i === index ? value.trim() === '' || value.trim() === data[index][property] : disabled,
+      ),
+    );
+  };
+
+  const handleSaveClick = () => {
+    if (buttonDisabled.some((disabled) => disabled)) {
+      displayMessage('Ошибка: одно или несколько полей заполнены некорректнo');
+    } else {
+      console.log('Измененные слова:', editedWords);
+      displayMessage('Изменения успешно сохранены');
+      setIsEditMode(false);
+    }
+  };
+
+  const handleCancelClick = () => {
+    setEditedWords(data.slice());
+    setIsEditMode(false);
+  };
+
+  const renderRows = () => {
+    return data.map((word, index) => {
+      const editedWord = editedWords[index];
+      const borderColor = buttonDisabled[index] ? 'red' : '';
+
+      return (
+        <tr key={index}>
+          {['english', 'transcription', 'russian'].map((property) => (
+            <td key={property}>
+              {isEditMode ? (
+                <input
+                  type="text"
+                  value={editedWord[property]}
+                  onChange={(e) => handleInputChange(e, index, property)}
+                  style={{ borderColor }}
+                />
+              ) : (
+                word[property]
+              )}
+            </td>
+          ))}
+          <td>
+            {isEditMode ? (
+              <>
+                <button
+                  className={`saveBtn${buttonDisabled[index] ? ' disabled' : ''}`}
+                  onClick={handleSaveClick}
+                  disabled={buttonDisabled[index]}>
+                  Сохранить
+                </button>
+                <button className="cancelBtn" onClick={handleCancelClick}>
+                  Отмена
+                </button>
+              </>
+            ) : (
+              <button
+                className="change-button"
+                type="button"
+                title="Редактировать слово"
+                onClick={() => toggleEditMode(index)}>
+                Изменить
+              </button>
+            )}
+            <button className="delete-button" title="Удалить слово">
+              Удалить
+            </button>
+          </td>
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <>
+      {message && (
+        <div className="MessageContainer">
+          <div className="Message">{message}</div>
+        </div>
+      )}
+      <div className="List">
+        <table>
+          <thead>
+            <tr>
+              <th>Слово</th>
+              <th>Транскрипция</th>
+              <th>Перевод</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>{renderRows()}</tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Words;
